1277c6e2dec05ddb45f7476976b4f823bdf3e566
WEBronckhorst <info@webronckhorst.nl>
2024-11-24 14:52:32 +0100

    Enable all users to access the panel
    The canAccessPanel function has been modified to always return true, allowing all users, regardless of their email domain or verification status, access to the panel. This change removes the previous restriction specific to 'webronckhorst.nl' email addresses.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,9 +11,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        if (!$this->app->environment('production')) {
-            $this->app->register('App\Providers\FakerServiceProvider');
-        }
+
     }
 
     /**
